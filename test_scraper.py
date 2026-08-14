122332ff979a0039907431943c457486b9f23cf1
Malva59 <boutillez.donovan@gmail.com>
2026-08-14 06:47:17 +0200

    Update test_scraper.py

--- test_scraper.py
+++ test_scraper.py
@@ -1,15 +1,3 @@
-from playwright.sync_api import sync_playwright
+from scraper import scrape_genshin_codes
 
-URL = "https://www.crimsonwitch.com/codes/Genshin_Impact"
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
-
-    print("Ouverture de Crimson Witch...")
-    page.goto(URL, wait_until="networkidle")
-
-    print("Titre :", page.title())
-    print("URL :", page.url)
-
-    browser.close()
+scrape_genshin_codes()

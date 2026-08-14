49cc947d7799a14c400f34f7997f1d9ff9c5a330
Malva59 <boutillez.donovan@gmail.com>
2026-08-14 06:55:19 +0200

    Update test_scraper.py

--- test_scraper.py
+++ test_scraper.py
@@ -1,5 +1,16 @@
 from scraper import scrape_genshin_codes
+from database import init_database, get_new_codes, save_code
+
+
+init_database()
 
 codes = scrape_genshin_codes()
 
-print(f"\nNombre de codes trouvés : {len(codes)}")
+new_codes = get_new_codes(codes)
+
+print(f"Codes trouvés : {len(codes)}")
+print(f"Nouveaux codes : {len(new_codes)}")
+
+for code in new_codes:
+    print(f"Nouveau : {code}")
+    save_code(code)
